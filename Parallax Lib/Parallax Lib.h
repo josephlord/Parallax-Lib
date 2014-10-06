0a41eb175f3538143f47bc253f40e8714d591d4b
Joseph Lord <joseph@human-friendly.com>
2014-10-06 19:22:31 +0100

    Initial Commit

---
+++ Parallax Lib/Parallax Lib.h
@@ -0,0 +1,19 @@
+//
+//  Parallax Lib.h
+//  Parallax Lib
+//
+//  Created by Joseph on 06/10/2014.
+//  Copyright (c) 2014 Swift London. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Parallax Lib.
+FOUNDATION_EXPORT double Parallax_LibVersionNumber;
+
+//! Project version string for Parallax Lib.
+FOUNDATION_EXPORT const unsigned char Parallax_LibVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Parallax_Lib/PublicHeader.h>
+
+
